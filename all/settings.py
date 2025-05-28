76a7e082456990980abbdb5b0a8a6b5448520955
Aramenkhose Wisdom <aramenkhosewisdom@gmail.com>
2025-05-28 14:12:31 +0100

    remove options

--- all/settings.py
+++ all/settings.py
@@ -87,10 +87,7 @@ DATABASES = {
         'USER': "postgres.yawzjqsjuqgggjafbins",
         'PASSWORD': "Zlsp3qAEGra5SdlO",
         'PORT': "5432",
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-            'charset': 'utf8mb4',
-        },
+      
     }
 }
 
